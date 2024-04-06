a80b6dd1b4e5800aa1339d5edcc965b9fd446250
Luccas Philot Goncalves <luccasgoncalvrs@gmail.com>
2024-04-06 14:59:50 -0300

    Alterção da primeira tela

--- bite-forms/src/app/app.component.ts
+++ bite-forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bite-forms';
+  novaPergunta: string = '';
+  alternativas: string[] = [''];
+  maxAlternativas: number = 10;
+
+  constructor(private router: Router) {}
+
+  criarPergunta() {
+    if (this.novaPergunta.trim() !== '' && this.alternativas.every(alternativa => alternativa.trim() !== '')) {
+      this.router.navigate(['/formulario']);
+    } else {
+      console.log('Por favor, preencha a pergunta e todas as alternativas.');
+    }
+  }
+
+  adicionarAlternativa() {
+    if (this.alternativas.length < this.maxAlternativas) {
+      this.alternativas.push('');
+    } else {
+      console.log('Limite máximo de alternativas atingido.');
+    }
+  }
 }
